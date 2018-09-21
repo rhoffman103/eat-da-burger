042c80d6e8dcdcbe4f2519fee6f9995f0ec2c2b1
Bobby H <rhoffman103@gmail.com>
2018-09-21 16:27:41 -0400

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+var express = require("express");
+
+// import the model to use its database functions
+var burger = require("../models/burger.js");
+
+var router = express.Router();
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers (burger_name) VALUES ('Sub-Conscious Burger (on a sub roll)');
+INSERT INTO burgers (burger_name) VALUES ('Sweet Home Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('I Mint to Do That Burger (comes with mint relish)');
+INSERT INTO burgers (burger_name) VALUES ('Take Me Out to the Burger (comes with peanuts & Cracker Jacks)');
+INSERT INTO burgers (burger_name) VALUES ('Grandpa Muenster Burger');
+INSERT INTO burgers (burger_name) VALUES ('Three Beets to the Wind Burger');
+INSERT INTO burgers (burger_name) VALUES ('Itsy Bitsy Teeny Weenie Yellow Polka-Dot Zucchini Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gourdon-Hamsey Burger (comes with squash & ham)');
+INSERT INTO burgers (burger_name) VALUES ('The Silentil Night Burger (comes with lentils)');
+INSERT INTO burgers (burger_name) VALUES ('Poblano Picasso Burger');
+INSERT INTO burgers (burger_name) VALUES ("Eggers Can't Be Cheesers (with fried egg & cheese)");
+INSERT INTO burgers (burger_name) VALUES ('Fifth Day of Christmas Burger (comes with five golden rings of onion)');
+INSERT INTO burgers (burger_name) VALUES ('Paranormal Pepper Jack-tivity Burger (comes with pepper Jack cheese)');
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar 'Bout Me Burger (comes with four kinds of cheddar)");
+INSERT INTO burgers (burger_name) VALUES ("Let's Give 'Em Something Shiitake 'Bout Burger");
